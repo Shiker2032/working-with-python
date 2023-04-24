db487ba1369eea1e01a37f8e194b8a118fb8e813
Vlad Begunov <53581923+Shiker2032@users.noreply.github.com>
2023-04-24 15:17:24 +0300

    input/output practice

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,28 @@
-print("Hello World")
+# task 1
+
+species = ""
+name = ""
+age = 0
+
+print("Input species")
+species = input()
+
+print("Input name")
+name = input()
+
+print("Input age")
+age = input()
+
+
+print("This is {} with the name of '{}', Age: {}".format(species, name, age))
+
+# task 2
+
+st1 = "Dryopithecus" 
+st2 = "Ramapithecus"
+st3 = "Australopithecus"
+st4 = "Home Erectus"
+st5 = "Homo Sapiens Neanderthalensis"
+st6 = "Homo Sapiens Sapiens"
+
+print(st1, st2, st3, st4, st5, st6, sep=" => ")
