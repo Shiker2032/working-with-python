284688c09ded98e77b58e32bd40a4c806142db86
Vlad Begunov <53581923+Shiker2032@users.noreply.github.com>
2023-04-25 14:04:37 +0300

    complete task

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,17 @@
-print("Hello World")
+# task 1
+print("Input base and height")
+b,h = map(int, input().split())
+s = (b * h) // 2
+print(s)
+
+#task 2
+a = 46275
+
+ones = a % 10
+tens = (a // 10) % 10
+hundreds = (a // 100) % 10
+thousands = (a // 1000) % 10
+tenThousands = (a // 10000) % 10
+
+result = ((tens ** ones) * hundreds) / (tenThousands - thousands)
+print(result)
