99a1191a8d04a5d71c1882fad4e197b58733c719
Vlad Begunov <53581923+Shiker2032@users.noreply.github.com>
2023-05-15 20:50:03 +0300

    complete task

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,20 @@
-print("Hello World")
+# task 1 check if polyndrome
+s1 = input("input polyndrome string ").lower()
+polyndrome = s1[::-1].lower()
+if (s1 == polyndrome):
+    print("yes")
+else:
+    print("no")
+
+
+#task 2 remove extra whitespaces
+s2 = "  ds   gb frurp  len s   are str   ab ho vi"
+s2 = s2.split()
+result = ""
+
+if (len(s2) > 1000):
+    print("Input shoul be lower that 1000 characters")
+else:
+    for i in s2:
+        result += i + " "
+    print(result)
